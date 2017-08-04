d8488eae396f3d09af48f76b863b3a8fa17a3a9d
wkm <wkmtem@163.com>
2017-08-04 15:59:39 +0800

    没加上

---
+++ UserCenter-core/src/main/java/com/compass/examination/constant/PushConstant.java
@@ -0,0 +1,23 @@
+package com.compass.examination.constant;
+
+
+/**
+ * 
+ * @Class Name: pushConstant
+ * @Description: 阿里推送常量
+ * @author: wkm
+ * @Company: www.compass.com
+ * @Create date: 2017年5月24日上午10:31:21
+ * @version: 2.0
+ */
+public class PushConstant {
+
+	// 推送配置
+	public static final String REGION_HANGZHOU = "cn-hangzhou";
+	public static final String ACCESSKEYID = "LTAIhz1MCIXE1z9R";
+	public static final String ACCESSKEYSECRET = "QYgzPvjzNzg8G7RyEtkSW28Vg8yBpX";
+	public static final long APPKEY = 23558121;
+	
+	// 账号推送
+	public final static String ACCOUNT = "account";
+}
